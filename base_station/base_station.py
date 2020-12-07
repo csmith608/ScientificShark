219d777978b30dbf1e9e589d16dae7d53fc5019a
Celeste <celestesmith@gatech.edu>
2020-12-07 15:09:00 -0500

    fixes to the ui and commenting code

--- base_station/base_station.py
+++ base_station/base_station.py
@@ -1,8 +1,5 @@
 # Import Python System Libraries
 import time
-from bs4 import BeautifulSoup
-import re
-import sys
 # Import Blinka Libraries
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -32,11 +29,13 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
 rfm9x.tx_power = 23
 prev_packet = None
 
-class Compass():
+
+class Compass:
     heading = 0
     pitchAngle = 0
     rollAngle = 0
     temperature = 0
+
 
 compassList = []
 bmsList = []
@@ -48,8 +47,6 @@ bmsFlag = False
 packetDone = False
 
 while True:
-    
-    packet = None
     # draw a box to clear the image
     # check for packet rx
     # can only receive 252 byte packets at a time
@@ -59,9 +56,9 @@ while True:
         display.fill(0)
         display.text('- received-', 15, 20, 1)
         display.show()
-        if(len(packet) == 1):
+        if len(packet) == 1:
             bmsFlag = True
-        elif(len(packet) == 2):
+        elif len(packet) == 2:
             packetDone = True
             bmsFlag = False
         # if flag is not set then build the compass packet
@@ -69,85 +66,69 @@ while True:
             bmsList.append(packet)
         else:
             compassList.append(packet)
-    
-    
+
     if packetDone:
         for c in range(0, len(compassList)):
             print("compass list: ", compassList[c])
         for b in range(0, len(bmsList)):
             print("diyBMS list:", bmsList[b])
-        #reset packet done
+        # reset packet done
         packetDone = False
-        
-        
+
+        # parse the compass information
+        # TODO: add error checking
         for c in range(len(compassList)):
-            
             line = compassList[c].decode('utf-8').rstrip()
-            '''
-            step_0 = line.split("C")
-            step_1 = step_0[1].split("P")
-            compass.heading = step_1[0]
-            List.append(compass.heading)
-            List.append(compass.heading)
-            
-            str1 = ""
-            str1 = str1.join(line)
-            '''
             compass = Compass()
-            compass.heading = line[line.find('$C')+len('$C'):line.rfind('P')]
+            compass.heading = line[line.find('$C') + len('$C'):line.rfind('P')]
             List.append(compass.heading)
-            compass.pitchAngle = line[line.find('P')+len('P'):line.rfind('R')]
+            compass.pitchAngle = line[line.find('P') + len('P'):line.rfind('R')]
             List.append(compass.pitchAngle)
-            compass.rollAngle = line[line.find('R')+len('R'):line.rfind('T')]
+            compass.rollAngle = line[line.find('R') + len('R'):line.rfind('T')]
             List.append(compass.rollAngle)
-            compass.temperature = line[line.find('T')+len('T'):line.rfind('*')]
+            compass.temperature = line[line.find('T') + len('T'):line.rfind('*')]
             List.append(compass.temperature)
-            # sending this data from one radio to other radio
-            
-            with open("compass_info.txt","a+") as f: #in write mode
+            with open("compass_info.txt", "a+") as f:  # in append mode
                 f.write("{}\n".format(List))
             f.close()
             List.clear()
-        #parse diyBMS information 
+        # parse diyBMS information
+        # TODO: add error checking
         for x in range(len(bmsList)):
             step_0 = bmsList[x].decode('utf-8').split(':')
-            if(step_0[0] == "R"):
+            if (step_0[0] == "R"):
                 step_1 = step_0[1]
                 step_2 = step_1.split("/")
-                if(step_2[1] == "83"):
+                if (step_2[1] == "83"):
                     step_3 = step_2[2]
                     step_4 = step_3.split("=")
-                    #first item is sequence number
-                    #second is data separated by spaces
+                    # first item is sequence number
+                    # second is data separated by spaces
                     temp = step_4[1].split(" ")
                     for t in range(16):
                         if t % 2 == 0:
-                            if(temp[t] != '0' and temp != '' and temp != ' '):
-                                temperature.append(int(temp[t], 16)/256 - 40)
-                            elif(temp[t] != ''):
-                                temperature.append(int(temp[t], 16))   
-                    with open("diybms_temperature.txt", "a") as f :
+                            if (temp[t] != '0' and temp != '' and temp != ' '):
+                                temperature.append(int(temp[t], 16) / 256 - 40)
+                            elif (temp[t] != ''):
+                                temperature.append(int(temp[t], 16))
+                    with open("diybms_temperature.txt", "a") as f:
                         for i in range(len(temperature)):
                             f.write(str(temperature[i]))
                             f.write(" ")
                         f.write("\n")
                     temperature.clear()
                     f.close
-                    #temp = temp[0]
-                    #turn temperature from str to int and from hex to decimal
-                    #temperature.append(int(temp, 16) - 40)
-                    #print(temp)
                     print("Cmd: 83")
-                elif(step_2[1] == "81"):
+                elif (step_2[1] == "81"):
                     step_3 = step_2[2]
                     step_4 = step_3.split("=")
-                    #first item is sequence number
-                    #second is data separated by spaces
+                    # first item is sequence number
+                    # second is data separated by spaces
                     volt = step_4[1].split(" ")
                     for t in range(len(volt)):
                         if t % 2 == 0 and volt[t] != 0 and volt[t] != '':
-                            voltage.append(int(volt[t], 16)/1000)
-                    with open("diybms_voltage.txt", "a") as f :
+                            voltage.append(int(volt[t], 16) / 1000)
+                    with open("diybms_voltage.txt", "a") as f:
                         for i in range(len(voltage)):
                             f.write(str(voltage[i]))
                             f.write(" ")
@@ -159,5 +140,5 @@ while True:
         compassList.clear
         List.clear
 
-        
+
 
--- base_station/ui.py
+++ base_station/ui.py
@@ -2,62 +2,75 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 
-fig = plt.figure(figsize=(20,10))
-ax1= fig.add_subplot(221)  
-ax2 = fig.add_subplot(222)  
-ax3 =fig.add_subplot(223)   
-ax4 =fig.add_subplot(224)
+# set the figure dimensions for each figure
+fig = plt.figure(figsize=(20, 10))
+ax1 = fig.add_subplot(221)
+ax2 = fig.add_subplot(222)
+ax3 = fig.add_subplot(223)
+ax4 = fig.add_subplot(224)
 
-fig2 = plt.figure(figsize=(20,10))
-ax5 =fig2.add_subplot(221)   
-ax6 =fig2.add_subplot(222)
+fig2 = plt.figure(figsize=(20, 10))
+ax5 = fig2.add_subplot(221)
+ax6 = fig2.add_subplot(222)
 
-listHeading=[]
-listRollangle=[]
-listPitchangle=[]
-listTemp=[]
-listCellVolt1=[]
-listCellVolt2=[]
-listCellVolt3=[]
-listCellVolt4=[]
-listCellVolt5=[]
-listCellVolt6=[]
-listCellVolt7=[]
-listCellVolt8=[]
-listCellTemp1=[]
-listCellTemp2=[]
-listCellTemp3=[]
-listCellTemp4=[]
-listCellTemp5=[]
-listCellTemp6=[]
-listCellTemp7=[]
-listCellTemp8=[]
+# declare each list
+listHeading = []
+listRollangle = []
+listPitchangle = []
+listTemp = []
+listCellVolt1 = []
+listCellVolt2 = []
+listCellVolt3 = []
+listCellVolt4 = []
+listCellVolt5 = []
+listCellVolt6 = []
+listCellVolt7 = []
+listCellVolt8 = []
+listCellTemp1 = []
+listCellTemp2 = []
+listCellTemp3 = []
+listCellTemp4 = []
+listCellTemp5 = []
+listCellTemp6 = []
+listCellTemp7 = []
+listCellTemp8 = []
 
+
+# function to parse and graph the compass data read in from the txt file created by the base station
 def animateCompass(i):
-    pullData = open("compass_info.txt","r").readlines()
+    # read the data from the file
+    pullData = open("compass_info.txt", "r").readlines()
+    # make sure there is enough data in the file before attempting to graph
+    while (len(pullData) < 2):
+        with open("compass_info.txt", "r") as f:
+            pullData = f.readlines()  # read lines from the file
+        print("Waiting for enough data to be be present")
+
+    # iterate through the file and parse the information contained
     for x in range(0, len(pullData)):
-	    firstLine = pullData[x]
-	    firstLine = firstLine.replace("'", '')
-	    firstLine = firstLine.replace(' ', '')
-	    firstLine = firstLine.replace(",", ' ')
-	    firstLine = firstLine.replace("[", '')
-	    firstLine = firstLine.replace("]", '')
-	    firstLine = firstLine.replace("\n", '')
-	    firstLine = firstLine.split(' ') #['102.6', '-2.1', '-54.6', '26.3']
-	    xs = range(0, len(pullData))
-	    print(firstLine)
-	    listHeading.append(float(firstLine[0]))
-	    listRollangle.append(float(firstLine[1]))
-	    listPitchangle.append(float(firstLine[2]))
-	    listTemp.append(float(firstLine[3]))
+        firstLine = pullData[x]
+        firstLine = firstLine.replace("'", '')
+        firstLine = firstLine.replace(' ', '')
+        firstLine = firstLine.replace(",", ' ')
+        firstLine = firstLine.replace("[", '')
+        firstLine = firstLine.replace("]", '')
+        firstLine = firstLine.replace("\n", '')
+        firstLine = firstLine.split(' ')  # ['102.6', '-2.1', '-54.6', '26.3']
+        # input for x coordinates
+        xs = range(0, len(pullData))
+        listHeading.append(float(firstLine[0]))
+        listRollangle.append(float(firstLine[1]))
+        listPitchangle.append(float(firstLine[2]))
+        listTemp.append(float(firstLine[3]))
+    # clear and create the graphs
     ax1.clear()
-    ax1.plot(xs,listHeading, 'cyan')
+    ax1.plot(xs, listHeading, 'cyan')
     ax2.clear()
-    ax2.plot(xs,listRollangle, 'red')
+    ax2.plot(xs, listRollangle, 'red')
     ax3.clear()
-    ax3.plot(xs,listPitchangle, 'purple')
+    ax3.plot(xs, listPitchangle, 'purple')
     ax4.clear()
-    ax4.plot(xs,listTemp, 'green')
+    ax4.plot(xs, listTemp, 'green')
     ax1.set_title('Heading vs reading')
     ax1.set_xlabel('reading')
     ax1.set_ylabel('Heading')
@@ -70,26 +83,26 @@ def animateCompass(i):
     ax4.set_title('Temperature vs reading')
     ax4.set_xlabel('reading')
     ax4.set_ylabel('Temperature')
+    # clear the lists for use on the next iteration
     listHeading.clear()
     listRollangle.clear()
     listPitchangle.clear()
     listTemp.clear()
 
+
+# function to parse and graph the cell voltage/temp data read in from the txt file created by the base station
 def animateBms(i):
-    with open("/home/pi/Desktop/diybms_temperature.txt", "r") as f: #reading cell data
-        celldatatemp = f.readlines()# ten lines 
-        print(celldatatemp)
-    
-    for yy in range(0,len(celldatatemp)):
+    with open("diybms_temperature.txt", "r") as f:  # reading cell data
+        celldatatemp = f.readlines()
+    # make sure there is enough data in the file before attempting to graph
+    while (len(celldatatemp) < 2):
+        with open("diybms_temperature.txt", "r") as f:
+            celldatatemp = f.readlines()  # read lines from the file
+        print("Waiting for enough data to be be present")
+    # parse the read in information and put in a list for graphing
+    for yy in range(0, len(celldatatemp)):
         tempLine = celldatatemp[yy]
-        tempLine = tempLine.replace("'", '')
-        #Line = Line.replace(' ', '')
-        tempLine = tempLine.replace(",", ' ')# 3.73 0.0 0.0 0.0 0.0 0.0 0.0 0.0 
-        tempLine = tempLine.replace("[", '')
-        tempLine = tempLine.replace("]", '')
-        tempLine = tempLine.replace("\n", '')
         tempLine = tempLine.split(' ')
-        print(tempLine[0])
         cellnumtemp = range(0, len(celldatatemp))
         listCellTemp1.append(float(tempLine[0]))
         listCellTemp2.append(float(tempLine[1]))
@@ -99,26 +112,20 @@ def animateBms(i):
         listCellTemp6.append(float(tempLine[5]))
         listCellTemp7.append(float(tempLine[6]))
         listCellTemp8.append(float(tempLine[7]))
-	
-    with open("/home/pi/Desktop/diybms_voltage.txt", "r") as f: #reading cell data
-        celldata = f.readlines()# ten lines 
 
-    while(len(celldata) < 2):
-        with open("/home/pi/Desktop/diybms_voltage.txt", "r") as f:
-            celldata = f.readlines()# read lines from the file 
+    with open("diybms_voltage.txt", "r") as f:  # reading cell data
+        celldata = f.readlines()  # ten lines
+    # make sure there is enough data in the file before attempting to graph
+    while (len(celldata) < 2):
+        with open("diybms_voltage.txt", "r") as f:
+            celldata = f.readlines()  # read lines from the file
         print("Waiting for enough data to be be present")
 
-    for xx in range(0,len(celldata)-1):
+    # parse the read in information and put in a list for graphing
+    for xx in range(0, len(celldata)):
         Line = celldata[xx]
-        Line = Line.replace("'", '')
-        #Line = Line.replace(' ', '')
-        Line = Line.replace(",", ' ')# 3.73 0.0 0.0 0.0 0.0 0.0 0.0 0.0 
-        Line = Line.replace("[", '')
-        Line = Line.replace("]", '')
-        Line = Line.replace("\n", '')
         Line = Line.split(' ')
-        print(Line[0])
-        cellnum = range(1, len(celldata))
+        cellnum = range(0, len(celldata))
         listCellVolt1.append(float(Line[0]))
         listCellVolt2.append(float(Line[1]))
         listCellVolt3.append(float(Line[2]))
@@ -128,29 +135,31 @@ def animateBms(i):
         listCellVolt7.append(float(Line[6]))
         listCellVolt8.append(float(Line[7]))
     ax5.clear()
-    ax5.plot(cellnumtemp,listCellTemp1, 'red')
-    ax5.plot(cellnumtemp,listCellTemp2, 'orange')
-    ax5.plot(cellnumtemp,listCellTemp3, 'yellow')
-    ax5.plot(cellnumtemp,listCellTemp4, 'green')
-    ax5.plot(cellnumtemp,listCellTemp5, 'blue')
-    ax5.plot(cellnumtemp,listCellTemp6, 'cyan')
-    ax5.plot(cellnumtemp,listCellTemp7, 'm')
-    ax5.plot(cellnumtemp,listCellTemp8, 'k')
+    ax5.plot(cellnumtemp, listCellTemp1, 'red')
+    ax5.plot(cellnumtemp, listCellTemp2, 'orange')
+    ax5.plot(cellnumtemp, listCellTemp3, 'yellow')
+    ax5.plot(cellnumtemp, listCellTemp4, 'green')
+    ax5.plot(cellnumtemp, listCellTemp5, 'blue')
+    ax5.plot(cellnumtemp, listCellTemp6, 'cyan')
+    ax5.plot(cellnumtemp, listCellTemp7, 'm')
+    ax5.plot(cellnumtemp, listCellTemp8, 'k')
+
     ax6.clear()
-    ax6.plot(cellnum,listCellVolt1, 'red')
-    ax6.plot(cellnum,listCellVolt2, 'orange')
-    ax6.plot(cellnum,listCellVolt3, 'yellow')
-    ax6.plot(cellnum,listCellVolt4, 'green')
-    ax6.plot(cellnum,listCellVolt5, 'blue')
-    ax6.plot(cellnum,listCellVolt6, 'cyan')
-    ax6.plot(cellnum,listCellVolt7, 'm')
-    ax6.plot(cellnum,listCellVolt8, 'k')
+    ax6.plot(cellnum, listCellVolt1, 'red')
+    ax6.plot(cellnum, listCellVolt2, 'orange')
+    ax6.plot(cellnum, listCellVolt3, 'yellow')
+    ax6.plot(cellnum, listCellVolt4, 'green')
+    ax6.plot(cellnum, listCellVolt5, 'blue')
+    ax6.plot(cellnum, listCellVolt6, 'cyan')
+    ax6.plot(cellnum, listCellVolt7, 'm')
+    ax6.plot(cellnum, listCellVolt8, 'k')
     ax5.set_title('Temperature vs reading')
     ax5.set_xlabel('reading')
     ax5.set_ylabel('Temp (degrees C)')
     ax6.set_title('Voltage vs reading')
     ax6.set_xlabel('reading')
     ax6.set_ylabel('Voltage (V)')
+    # clear the lists for use in the next iteration
     listCellTemp1.clear()
     listCellTemp2.clear()
     listCellTemp3.clear()
@@ -159,7 +168,18 @@ def animateBms(i):
     listCellTemp6.clear()
     listCellTemp7.clear()
     listCellTemp8.clear()
-	
+    listCellVolt1.clear()
+    listCellVolt2.clear()
+    listCellVolt3.clear()
+    listCellVolt4.clear()
+    listCellVolt5.clear()
+    listCellVolt6.clear()
+    listCellVolt7.clear()
+    listCellVolt8.clear()
+
+
+# use the animate function so it will check the files for new information every 1 second
+# (this should probably be turned up)
 ani = animation.FuncAnimation(fig, animateCompass, interval=1000)
 aniBms = animation.FuncAnimation(fig2, animateBms, interval=1000)
 plt.show()
